c745d023e8245ae3f0fb2c4a4969b316dfed2a59
tineprogramming <none_reply@live.com>
2023-02-05 04:46:21 +0700

    Update fordpass_new.py

--- custom_components/fordpass/fordpass_new.py
+++ custom_components/fordpass/fordpass_new.py
@@ -29,6 +29,7 @@ region_lookup = {
     "UK&Europe": "1E8C7794-FF5F-49BC-9596-A1E0C86C5B19",
     "Australia": "5C80A6BB-CF0D-4A30-BDBF-FC804B5C1A98",
     "North America & Canada": "71A3AD0A-CF46-4CCF-B473-FC7FE5BC4592",
+    "Thailand": "39CD6590-B1B9-42CB-BEF9-0DC1FDB96260",
 }
 
 baseUrl = "https://usapi.cv.ford.com/api"
